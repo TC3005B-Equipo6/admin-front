fe7a9909759cc1829824d2b79babff9a91546244
OmarERz <A01665451@tec.mx>
2026-04-24 17:25:24 -0600

    feature/Add button to make password visible

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,19 +1,19 @@
 import { useState, type SyntheticEvent } from "react";
 import { useNavigate } from "react-router-dom";
-import { Input } from "../../components/Input/Input";
 import { Button } from "../../components/Button/Button";
-import styles from "./Login.module.css";
+import { Input } from "../../components/Input/Input";
 import { login } from "../../services/authService";
+import styles from "./Login.module.css";
 
 export default function LoginScreen() {
   const navigate = useNavigate();
 
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
-
   const [error, setError] = useState("");
 
-  const handleSubmit = async (e: SyntheticEvent<HTMLFormElement>) => { e.preventDefault();
+  const handleSubmit = async (e: SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
     if (!user || !password) {
       setError("Completa todos los campos");
@@ -26,11 +26,9 @@ export default function LoginScreen() {
       const token = await login(user, password);
 
       localStorage.setItem("token", token);
-
       navigate("/home");
     } catch {
       setError("Usuario o contraseña incorrectos");
-    } finally {
     }
   };
 
@@ -38,47 +36,35 @@ export default function LoginScreen() {
     <div className={styles.wrapper}>
       <div className={styles.card}>
         <div className={styles.left}>
-          <img
-            src="/move360.png"
-            alt="move360"
-            className={styles.logo}
-          />
+          <img src="/move360.png" alt="move360" className={styles.logo} />
 
           <h1>BIENVENIDO</h1>
 
-          <form
-            className={styles.form}
-            onSubmit={handleSubmit}
-          >
+          <form className={styles.form} onSubmit={handleSubmit}>
             <Input
               label="Usuario"
               placeholder="ejemplo@move360.com"
               value={user}
-              onChange={(e) =>
-                setUser(e.currentTarget.value)}
+              onChange={(e) => setUser(e.currentTarget.value)}
             />
 
             <Input
               label="Contraseña"
               type="password"
+              autoComplete="current-password"
+              showPasswordToggle
               value={password}
-              onChange={(e) =>
-              setPassword(e.currentTarget.value)}
+              onChange={(e) => setPassword(e.currentTarget.value)}
             />
 
-            {error && (
-              <p className={styles.error}>
-                {error}
-              </p>
-            )}
+            {error && <p className={styles.error}>{error}</p>}
 
-            <Button type="submit" 
-            label={"INICIAR SESIÓN"}
-            />
+            <Button type="submit" label="INICIAR SESIÓN" />
 
-            <p>   ¿Olvidaste tu contraseña?</p>
+            <p>¿Olvidaste tu contraseña?</p>
           </form>
         </div>
+
         <div className={styles.right}></div>
       </div>
     </div>
