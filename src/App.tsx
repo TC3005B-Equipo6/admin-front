fb3cd7ac9e2cbf8be90fac455f3dc4dc599080f6
saraxfl <A07045944@tec.mx>
2026-06-04 13:03:21 -0600

    feat:Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css'
 import Login from "./screens/Login/Login";
-import Home from "./screens/Home/Home"; 
+import { Home } from "./screens/Home/Home";
 
 
 function App() {
